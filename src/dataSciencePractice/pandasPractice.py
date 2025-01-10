164debd2df586a379737a1e25905bd52f8c9e98e
Uncode-Alpha <geimsyael@hotmail.com>
2025-01-10 01:48:21 -0600

    Operating with Dataframes & Excel files

--- src/dataSciencePractice/pandasPractice.py
+++ src/dataSciencePractice/pandasPractice.py
@@ -5,6 +5,7 @@
 
 #Importing the pandas library
 import pandas as pd
+import numpy as np
 
 def exampleDataframe():
     # 1. Using list of dictionary
@@ -32,7 +33,6 @@ def exampleDataframe():
 
 def excercise1():
     #Given following dataframe
-    import pandas as pd
     df = pd.DataFrame([[0.23,'f1'],[5.36,'f2']],index = list('pq'),
                     columns = list('ab'))
     #1 Change the column name from 'a' to 'A'
@@ -40,9 +40,75 @@ def excercise1():
     print('Dataframe after changing column name: \n',df)
     
     #2 Add a new column 'c' with random values
+    df.insert(2,'c',pd.Series(np.random.randn(2), index=df.index))
+    print('Dataframe after adding new column: \n',df)
+    
     #3 Change the datatype of column 'A' values to complex
+    df['A'] = df['A'].astype(complex)
+    print('Dataframe after changing datatype \n',df)
+    
     #4 Display rows whose any of the element matches with any element of
     #the given list
     lst = ['f30','f50','f2','f0'] 
+    print('Printing only the row that matches at least one element of the list\n'
+            ,df[df.isin(lst).any(axis=1)])
+    #Now we will print only the index of the rows that match the list
+    print('Printing only the index of the rows that match the list\n'
+            ,df.index[df.isin(lst).any(axis=1)])
+    
 
-excercise1()
+def excelExample():
+    #This code needs pandas and openpyxl installed
+    #This part of the example writes a dataset to an xlsx format
+    df = pd.DataFrame([[11, 202],[33, 44]],index = list('AB'),columns = list('CD'))
+    #Here we can specify the path and name of the file to put our dataframe info
+    ''' Writing to excel file '''
+    df.to_excel('data/dataSciencePractice/test_file.xlsx', sheet_name = 'Sheet1')
+    ''' Reading from excel file '''
+    print(pd.read_excel('data/dataSciencePractice/test_file.xlsx', 'Sheet1'))
+    #Woth this command we can only print specific rows of a table
+    print(df.head(1)) 
+    #This part of the excel explains using read_excel to read a table from a file
+    #df = pd.read_table('chat.txt')
+    return
+
+def excercise2():
+    #Given this dataframe, perform the following
+    dataTable = pd.DataFrame([[18,10,5,11,-2],
+                    [2,-2,9,-11,3],
+                    [-4,6,-19,2,1],
+                    [3,-14,1,-2,8],
+                    [-2,2,4,6,13]],
+                    index = list('pqrst'),
+                    columns = list('abcde'))
+    #Excercise 1
+    #Extract the rows whose total sum is even. 
+    #Next, save this dataframe into a new dataframe named newTable . 
+    #Copy this dataframe to an excel file named file_df_excercise2.xlsx under Sheet 1.
+    #Solution: We show the sum
+    print('Sum of rows from datTable\n',dataTable.sum(axis=1))
+    #we perform  the operation  of assigning the rows that sum is even using % operator
+    #we use dataTable to operate over all the rows
+    newTable=dataTable[dataTable.sum(axis=1)%2 == 0]
+    print('New table with rows that sum is even\n',newTable)
+    #Lastly we write the result in the excel file
+    newTable.to_excel('data/dataSciencePractice/file_df_excercise2.xlsx', 
+                    sheet_name = 'Sheet1',index_label='Even sum rows')
+    #We read the excel as proof of the operation
+    print('This is the excel with only even rows\n',
+        pd.read_excel('data/dataSciencePractice/file_df_excercise2.xlsx','Sheet1'))
+    
+    #Excercise 2
+    #Copy newTable dataframe into a new dataframe df_temp. 
+    #Append a new column named as 'm' to the df_temp dataframe which defines the multiplication of each element in one row.
+    #Save this dataframe in Sheet 2 of file_df excel file.
+    df_temp = newTable.copy()
+    df_temp['m']=df_temp.apply(lambda x: x.prod(), axis=1)
+    print('Temporal dataframe with new column m\n',df_temp)
+    #We add the result to the excel file
+    with pd.ExcelWriter('data/dataSciencePractice/file_df_excercise2.xlsx', engine='openpyxl', mode='a') as writer:
+        df_temp.to_excel(writer, sheet_name='Sheet2', index_label='Multiplication of elements')
+    print('New excel sheet with added column\n',pd.read_excel('data/dataSciencePractice/file_df_excercise2.xlsx','Sheet2'))
+    return
+
+excercise2()
